6ad055d00bfbc75003e2da90cca4be99450d3dea
waker <wakeroid@gmail.com>
2011-06-15 09:19:22 +0200

    ape: arm neon optimizations

--- plugins/ffap/Android.mk
+++ plugins/ffap/Android.mk
@@ -6,6 +6,11 @@ LOCAL_MODULE := ffap
 LOCAL_SRC_FILES += ffap.c
 
 LOCAL_CFLAGS += -I$(LOCAL_PATH)/../.. -std=c99 -O2
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+	LOCAL_ARM_NEON := true
+	LOCAL_SRC_FILES += int_neon.S
+	LOCAL_CFLAGS += -mfpu=neon -DARCH_ARM=1
+endif
 
 LOCAL_ARM_MODE := arm
 
