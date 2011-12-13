9abd6f61f26860afec3a83e0280d149895caeb00
waker <wakeroid@gmail.com>
2011-12-13 20:11:26 +0100

    [android] get rid of static zlib

--- plugins/vfs_zip/Android.mk
+++ plugins/vfs_zip/Android.mk
@@ -5,11 +5,12 @@ LOCAL_MODULE := vfs_zip
 
 LOCAL_SRC_FILES += vfs_zip.c
 
-LOCAL_STATIC_LIBRARIES := zip z
+LOCAL_STATIC_LIBRARIES := zip
 
 LOCAL_CFLAGS += -I$(LOCAL_PATH)/../.. -I$(LOCAL_PATH)/../../android/jni/libzip/lib -O2 -std=c99
 
 LOCAL_ARM_MODE := arm
+LOCAL_LDLIBS    := -lz
 
 include $(BUILD_SHARED_LIBRARY)
 
