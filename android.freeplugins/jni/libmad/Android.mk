35f83df56f2b7538c62095565641e27597cdebec
waker <wakeroid@gmail.com>
2012-01-22 15:39:43 +0100

    [android] suspected fix to mp3 crash on some devices

--- android.freeplugins/jni/libmad/Android.mk
+++ android.freeplugins/jni/libmad/Android.mk
@@ -7,9 +7,6 @@ LOCAL_SRC_FILES += bit.c decoder.c fixed.c frame.c huffman.c layer12.c layer3.c 
 
 LOCAL_CFLAGS += -DFPM_ARM -DASO_IMDCT -O2
 
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-	LOCAL_CFLAGS += -mfpu=neon -fpic -fno-signed-zeros
-endif
 LOCAL_ARM_MODE := arm
 
 include $(BUILD_STATIC_LIBRARY)
